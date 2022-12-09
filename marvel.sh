29436c3175e092ec626750d743ad210c2800b3c7
Stephen Cleary <Steve0cleary@gmail.com>
2022-12-09 19:20:54 +0000

    apache script

--- marvel.sh
+++ marvel.sh
@@ -1,10 +1,10 @@
 #!bin/bash/
-#installing packages
+#installing packagess
 yum update -y
 yum install -y httpd
 systemctl start httpd
 systemctl enable httpd
-
+#editing html
 cd /var/www/html
 
 sudo echo "<h1>Red Team, Let's go! </h1>" > index.html
