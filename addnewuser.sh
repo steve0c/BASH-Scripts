384a7c4a8985717cd193a98ba6684aff1fca5eb4
EC2 Default User <ec2-user@ip-172-31-89-248.ec2.internal>
2022-12-12 12:38:24 +0000

    script for adding and checking new users

---
+++ addnewuser.sh
@@ -0,0 +1,20 @@
+#!/bin/bash 
+echo "Enter the name of the new user:"
+
+read username
+  
+#checking if the user exists
+if  
+	getent passwd "$username" >/dev/null 
+			then
+						echo "Username already exists"
+						#if the user name does not exist it will be added i
+						else
+							sudo useradd -m $username
+									echo "$username has been created"
+							sudo passwd $username
+											
+									fi
+									#Showing User ID Group Id and Group 
+									echo "Here is additional information for $username"       
+										id $usernamei
